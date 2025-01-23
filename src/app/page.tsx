5d5d3e9b5842640518354793e15c0a2f9bb55f88
Bhuv <bhuv059@gmail.com>
2025-01-23 11:06:56 +0100

    navbar_mobile_desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-"use client";
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  const handleReload = () => {
-    window.location.reload();
-  };
-
-  return (
-    <div className="mt-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn ng-show={handleReload}>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-    </div>
-  );
+  return <div className="mt-4">test</div>;
 }
